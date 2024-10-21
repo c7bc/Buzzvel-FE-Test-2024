7f396466f16dc0d259c0179c0a912b65e3776a70
Daniel <dnnxzz70@gmail.com>
2024-10-21 15:01:19 -0300

    modified:   next.config.mjs 	modified:   package-lock.json 	modified:   package.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+import withBundleAnalyzer from '@next/bundle-analyzer';
+
 const nextConfig = {
-  compress: true,
+  compress: true,  // Mantém a compressão habilitada
+  reactStrictMode: true,  // Modo estrito do React
 };
 
-export default nextConfig;
+export default withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',  // Ativa o Bundle Analyzer quando ANALYZE=true
+})(nextConfig);
