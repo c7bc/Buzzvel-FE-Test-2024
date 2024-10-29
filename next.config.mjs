c7bd280079cf8dcec5e0cf77b923de7543abb893
Daniel <dnnxzz70@gmail.com>
2024-10-29 11:57:43 -0300

    modified:   analyze/nodejs.html 	modified:   next.config.mjs 	modified:   src/components/Sections/PremiumServices.tsx

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
-/** @type {import('next').NextConfig} */
-import withBundleAnalyzer from '@next/bundle-analyzer';
+// /** @type {import('next').NextConfig} */
+// import withBundleAnalyzer from '@next/bundle-analyzer';
 
-const nextConfig = {
-  compress: true,
-  reactStrictMode: true,
-  swcMinify: true,
-};
+// const nextConfig = {
+//   compress: true,
+//   reactStrictMode: true,
+//   swcMinify: true,
+// };
 
-export default withBundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true',
-})(nextConfig);
+// export default withBundleAnalyzer({
+//   enabled: process.env.ANALYZE === 'true',
+// })(nextConfig);
